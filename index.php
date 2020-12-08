62a06ec74b0cbba19284b0b503f76906981a2ae8
Max <maxblanck@outlook.fr>
2020-12-08 11:20:32 +0100

    php Partie 1  Exercice 8

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 8 Partie 1</title>
+</head>
+<body>
+    <h1>Exercice 8 Partie 1</h1>
+
+    <?php
+    $add = 3 + 4;
+    $multi = 5 * 20;
+    $divi = 45 / 5;
+    echo $add . ' ' . $multi . ' ' . $divi;
+    ?>
+</body>
+</html>
