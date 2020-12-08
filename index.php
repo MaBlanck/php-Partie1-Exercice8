fa6e56cf596df3fcec7eac7d59807afeaad4aca9
Max <maxblanck@outlook.fr>
2020-12-08 12:32:11 +0100

    php Partie 1  Exercice 8

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
+<?php
+    $add = 3 + 4;
+    $multi = 5 * 20;
+    $divi = 45 / 5;
+    ?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Exercice 8 Partie 1</title>
 </head>
+
 <body>
     <h1>Exercice 8 Partie 1</h1>
-
-    <?php
-    $add = 3 + 4;
-    $multi = 5 * 20;
-    $divi = 45 / 5;
-    echo $add . ' ' . $multi . ' ' . $divi;
-    ?>
+    <p><?= $add . ' ' . $multi . ' ' . $divi ?></p>
 </body>
+
 </html>
